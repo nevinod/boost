8bd3f16c36431dbcaf7e2bbd55d2fbb38d38786f
Neil Vinod <neilvinod14@gmail.com>
2018-12-11 17:39:27 -0800

    model controllers

--- app/controllers/problems_controller.rb
+++ app/controllers/problems_controller.rb
@@ -1,51 +1,17 @@
-class ProblemsController < ApplicationController
+class ProblemsController < ApiController
   before_action :set_problem, only: [:show, :update, :destroy]
 
   # GET /problems
   def index
-    @problems = Problem.all
+    @problems = Problem.select("id, question").all
 
-    render json: @problems
+    render json: @problems.to_json
   end
 
   # GET /problems/1
   def show
-    render json: @problem
+    @problem = Problem.find(params[:id])
+    render json: @problem.to_json(:include => { :answers => { :only => [:id, :text, :value] }})
   end
 
-  # POST /problems
-  def create
-    @problem = Problem.new(problem_params)
-
-    if @problem.save
-      render json: @problem, status: :created, location: @problem
-    else
-      render json: @problem.errors, status: :unprocessable_entity
-    end
-  end
-
-  # PATCH/PUT /problems/1
-  def update
-    if @problem.update(problem_params)
-      render json: @problem
-    else
-      render json: @problem.errors, status: :unprocessable_entity
-    end
-  end
-
-  # DELETE /problems/1
-  def destroy
-    @problem.destroy
-  end
-
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_problem
-      @problem = Problem.find(params[:id])
-    end
-
-    # Only allow a trusted parameter "white list" through.
-    def problem_params
-      params.require(:problem).permit(:question, :difficulty, :answer)
-    end
 end
